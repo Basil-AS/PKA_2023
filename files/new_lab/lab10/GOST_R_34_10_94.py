b8b9928d9a8b502e954287d77cd9fcda90e78cda
Vasilii S <vasiliiskrypnik02@gmail.com>
2023-06-07 02:41:55 +0300

    add files

--- files/new_lab/lab10/GOST_R_34_10_94.py
+++ files/new_lab/lab10/GOST_R_34_10_94.py
@@ -1,65 +1,61 @@
-
 from typing import Union
-
-
-from script.const import REPLACES, ABC
-from script.method import to_indexes, get_hash, clear_text, replace_special
+from script.const import REPLACES, ABC  # Импортируем константы REPLACES и ABC из модуля script.const
+from script.method import to_indexes, get_hash, clear_text, replace_special  # Импортируем функции из модуля script.method
 
 
 def primes(n):
     """
     Факторизация числа n
     """
-    primfac = []
+    primfac = []  # Создаем пустой список для хранения простых множителей
     d = 2
     while d * d <= n:
         while (n % d) == 0:
-            primfac.append(d)  # supposing you want multiple factors repeated
-            n //= d
+            primfac.append(d)  # Добавляем простой множитель в список
+            n //= d  # Делим число на простой множитель
         d += 1
     if n > 1:
         primfac.append(n)
-    return primfac
+    return primfac  # Возвращаем список простых множителей
 
 
 def enc(text: str, P: str, Q: str, A: str, X: str, K: str, h: int = None):
-    P, Q, A, X, K = map(int, (P, Q, A, X, K))
-    assert Q in primes(P - 1), "Q должно быть множителем для P-1"
-    assert (A ** Q) % P == 1, "Условие (a**q)%p==1 не соблюдено"
-    assert 1 < A < (P - 1), "A должно ∈ (1, P-1)"
-    assert X < Q, "X должно быть < Q"
-    assert K < Q, "K должно быть < Q"
+    P, Q, A, X, K = map(int, (P, Q, A, X, K))  # Преобразуем входные аргументы в целочисленные значения
+    assert Q in primes(P - 1), "Q должно быть множителем для P-1"  # Проверяем, что Q является множителем для P-1
+    assert (A ** Q) % P == 1, "Условие (a**q)%p==1 не соблюдено"  # Проверяем выполнение условия (a**q)%p==1
+    assert 1 < A < (P - 1), "A должно ∈ (1, P-1)"  # Проверяем, что A находится в интервале (1, P-1)
+    assert X < Q, "X должно быть < Q" 
+    assert K < Q, "K должно быть < Q" 
 
-    text = clear_text(text)
+    text = clear_text(text)  # Очищаем текст от специальных символов и приводим к нижнему регистру
 
-    text_hash = get_hash(text, Q, h)
+    text_hash = get_hash(text, Q, h)  # Вычисляем хэш текста
 
-    r = ((A ** K) % P) % Q
-    assert r != 0, "R не должен = 0. Попробуйте ввести другие параметры"
+    r = ((A ** K) % P) % Q  # Вычисляем значение r
+    assert r != 0, "R не должен = 0. Попробуйте ввести другие параметры"  # Проверяем, что r не равно 0
 
-    s = (X * r + K * text_hash) % Q
+    s = (X * r + K * text_hash) % Q  # Вычисляем значение s
     return f"{r},{s}"
 
 
 # y = (pub_key.a ** x) % pub_key.p
 def dec(text: str, P: str, Q: str, A: str, Y: str, ecp: str, h: int = None):
-    r, s = map(int, ecp.split(","))
-    P, Q, Y, A = map(int, (P, Q, Y, A))
-    text = clear_text(text)
+    r, s = map(int, ecp.split(","))  # Разделяем ecp на значения r и s
+    P, Q, Y, A = map(int, (P, Q, Y, A))  # Преобразуем входные аргументы в целочисленные значения
+    text = clear_text(text)  # Очищаем текст от специальных символов и приводим к нижнему регистру
 
-    hashed_m = get_hash(text, Q, h)
+    hashed_m = get_hash(text, Q, h)  # Вычисляем хэш текста
 
-    v = (hashed_m ** (Q - 2)) % Q
-    z1 = (s * v) % Q
+    v = (hashed_m ** (Q - 2)) % Q 
+    z1 = (s * v) % Q  
     z2 = ((Q - r) * v) % Q
     u = ((A ** z1 * Y ** z2) % P) % Q
 
-    return u == r
+    return u == r  # Возвращаем результат сравнения u и r
 
 
 def main():
-    text_test = input("Введите пословицу: ")
-
+    text_test = input("Введите пословицу: ")  # Вводим текст пословицы
 
     def print_kv(k, v):
         print(kv(k, v))
@@ -67,25 +63,22 @@ def main():
     def kv(k, v):
         return f"[bold cyan] {k} :[/bold cyan] {v} "
 
-    #p, q, a = 23, 11, 6
-    #k = 5
-    #x = 8
-
     p = int(input("Введите число для переменной p: "))
     q = int(input("Введите число для переменной q: "))
     a = int(input("Введите число для переменной a: "))
     k = int(input("Введите число для переменной k: "))
     x = int(input("Введите число для переменной x: "))
-    y = (a ** x) % p
+    y = (a ** x) % p  # Вычисляем значение y
 
-    ecp = enc(text_test, p, q, a, x, k)
-    chk = dec(text_test, p, q, a, y, ecp)
-    print("Подпись пословицы", ecp)
-    print("Проверка подписи", chk)
+    ecp = enc(text_test, p, q, a, x, k)  # Шифруем текст пословицы
+    chk = dec(text_test, p, q, a, y, ecp)  # Расшифровываем текст пословицы и проверяем подпись
+    print("\nПодпись пословицы", ecp)
+    
+    if chk:
+        print("Подпись верна")
+    else: print("Подпись неверна")
 
 
-    print("Проверка подписи", chk)
-
-
+#47 23 3 3 7
 if __name__ == "__main__":
     main()
--- files/new_lab/lab9/RSA.py
+++ files/new_lab/lab9/RSA.py
@@ -1,146 +1,167 @@
-import random
-from hashlib import sha256
+alphabet = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п',
+            'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
 
+# Функция, которая преобразует числа в буквы
+def numberToLetter(text):
+    strInLetter = ''
+    for i in text:
+        if i == 0:
+            strInLetter += ' '
+        else:
+            strInLetter += alphabet[i-1]
+    return strInLetter
 
-def coprime(a, b):
+# Функция, которая заменяет символы в тексте
+def replacer(text):
+    text = text.replace('ё', 'е')
+    text = text.replace(',', 'зпт').replace('.', 'тчк').replace('-', 'тире')
+    text = text.lower()
+    return text
+
+# Функция, которая преобразует буквы в числа
+def letterToNumber(text):
+    strInNumber = []
+    if type(text) is str:
+        text = replacer(text)
+    for letter in text:
+        if letter == ' ':
+            strInNumber.append(0)
+        else:
+            strInNumber.append((alphabet.index(letter) + 1))
+    return strInNumber
+
+# Функция, которая проверяет число на простоту
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+# Функция, которая находит НОД двух чисел
+def gcd(a, b):
     while b != 0:
         a, b = b, a % b
     return a
 
+# Функция, которая находит мультипликативно обратный элемент по модулю
+def multiplicative_inverse(e, phi):
+    d = 0
+    x1 = 0
+    x2 = 1
+    y1 = 1
+    temp_phi = phi
 
-def extended_gcd(aa, bb):
-    lastremainder, remainder = abs(aa), abs(bb)
-    x, lastx, y, lasty = 0, 1, 1, 0
-    while remainder:
-        lastremainder, (quotient, remainder) = remainder, divmod(lastremainder, remainder)
-        x, lastx = lastx - quotient * x, x
-        y, lasty = lasty - quotient * y, y
-    return lastremainder, lastx * (-1 if aa < 0 else 1), lasty * (-1 if bb < 0 else 1)
+    while e > 0:
+        temp1 = temp_phi // e
+        temp2 = temp_phi - temp1 * e
+        temp_phi = e
+        e = temp2
 
+        x = x2 - temp1 * x1
+        y = d - temp1 * y1
 
-# Euclid's extended algorithm for finding the multiplicative inverse of two numbers
-def modinv(a, m):
-    g, x, y = extended_gcd(a, m)
-    if g != 1:
-        raise Exception('Modular inverse does not exist')
-    return x % m
+        x2 = x1
+        x1 = x
+        d = y1
+        y1 = y
 
+    if temp_phi == 1:
+        return d + phi
 
-def is_prime(num):
-    if num == 2:
-        return True
-    if num < 2 or num % 2 == 0:
-        return False
-    for n in range(3, int(num ** 0.5) + 2, 2):
-        if num % n == 0:
-            return False
-    return True
-
-
-def generate_keypair(p, q):
-    if not (is_prime(p) and is_prime(q)):
-        raise ValueError('Both numbers must be prime.')
-    elif p == q:
-        raise ValueError('p and q cannot be equal')
+# Функция, которая генерирует открытый и закрытый ключи
+def generate_keypair(p: int, q: int):
+    while not (is_prime(p) and is_prime(q) and p != q):
+        print('p и/или q не простое число или p = q')
+        p = int(input('Введите p: '))
+        q = int(input('Введите q: '))
 
     n = p * q
+    phi = (p-1) * (q-1)
+    print('Функция Эйлера равна: ', phi)
 
-    # Phi is the totient of n
-    phi = (p - 1) * (q - 1)
+    e = int(input('Введите e: '))
+    while gcd(e, phi) != 1 or e < 1 or e > phi:
+        print('Не выполняются условия: 1 < e < phi и e с phi взаимно просты')
+        e = int(input('Введите e: '))
 
-    # Choose an integer e such that e and phi(n) are coprime
-    e = random.randrange(1, phi)
+    d = multiplicative_inverse(e, phi)
 
-    # Use Euclid's Algorithm to verify that e and phi(n) are comprime
-    g = coprime(e, phi)
-
-    while g != 1:
-        e = random.randrange(1, phi)
-        g = coprime(e, phi)
-
-    # Use Extended Euclid's Algorithm to generate the private key
-    d = modinv(e, phi)
-
-    # Return public and private keypair
-    # Public key is (e, n) and private key is (d, n)
     return ((e, n), (d, n))
 
-
-def encrypt(privatek, plaintext):
-    # Unpack the key into it's components
-    key, n = privatek
-
-    # Convert each letter in the plaintext to numbers based on the character using a^b mod m
-
-    numberRepr = [ord(char) for char in plaintext]
-    print("Number representation before encryption: ", numberRepr)
-    cipher = [pow(ord(char), key, n) for char in plaintext]
-
-    # Return the array of bytes
+# Функция шифрования RSA
+def encrypt_rsa(pk, plaintext):
+    cipher = []
+    key, n = pk
+    for char in plaintext:
+        cipher.append(pow(char, key, n))
     return cipher
 
+# Функция расшифрования RSA
+def decrypt_rsa(pk, ciphertext):
+    plain = []
+    key, n = pk
+    for char in ciphertext:
+        plain.append((pow(char, key, n)))
+    return plain
 
-def decrypt(publick, ciphertext):
-    # Unpack the key into its components
-    key, n = publick
+# Функция, которая применяет RSA к тексту
+def crypt_rsa(pk, beforetext):
+    aftertext = []
+    key, n = pk
+    for char in beforetext:
+        aftertext.append((pow(char, key, n)))
+    return aftertext
 
-    # Generate the plaintext based on the ciphertext and key using a^b mod m
-    numberRepr = [pow(char, key, n) for char in ciphertext]
-    plain = [chr(pow(char, key, n)) for char in ciphertext]
+# Функция хеширования
+def hash(phr, mod):
+    h=[0]
+    for ind, i in enumerate(phr, 1):
+        t = ((h[ind-1]+alphabet.index(i)+1)**2) % mod
+        h.append(t)
+    if h[-1] == 0:
+        h.append(1)
+    return(h)
 
-    print("Decrypted number representation is: ", numberRepr)
+def rsa():
+    p = 11
+    q = 3
+    public_key, private_key = generate_keypair(p, q)
+    print('e, n =', public_key, 'd, n =', private_key)
 
-    # Return the array of bytes as a string
-    return ''.join(plain)
+    message = "времязптприливыиотливынеждутчеловекатчк"
+    num_message = letterToNumber(message)
+    print('num message: ', num_message)
+    encrypted_message = encrypt_rsa(public_key, num_message)
+    print("Зашифрованный текст:", encrypted_message)
+    print("Зашифрованный текст:", numberToLetter(encrypted_message))
 
+    decrypted_message = decrypt_rsa(private_key, encrypted_message)
+    print("\nРасшифрованный текст:", decrypted_message)
+    print("\nРасшифрованный текст:", numberToLetter(decrypted_message))
 
-def hashFunction(message):
-    hashed = sha256(message.encode("UTF-8")).hexdigest()
-    return hashed
+def rsa_sign():
+    message = input("Введите текст: ")
 
+    p = int(input("Введите p: "))
+    q = int(input("Введите q: "))
+    public_key, private_key = generate_keypair(p, q)
+    print('e, n =', public_key, 'd, n =', private_key)
 
-def verify(receivedHashed, message):
-    ourHashed = hashFunction(message)
-    if receivedHashed == ourHashed:
-        print("Verification successful: ", )
-        print(receivedHashed, " = ", ourHashed)
-    else:
+    hash_message = []
+    hash_message.append(hash(message, 47)[-1])
+    print('hash message: ', *hash_message)
+    signed_message = crypt_rsa(private_key, hash_message)
+    print("Подпись: ", *signed_message, public_key[-1])
 
-        print("Verification failed")
-        print(receivedHashed, " != ", ourHashed)
+    checked_message = crypt_rsa(public_key, signed_message)
+    print("\nПроверка подписи: ", *checked_message)
+    if hash_message == checked_message:
+        print("Подпись верна")
+    else: print("Подпись неверна")
 
+#47 13 13 
 
-def main():
-    p = int(input("Enter a prime number (17, 19, 23, etc): "))
-    q = int(input("Enter another prime number (Not one you entered above): "))
-    # p = 17
-    # q=23
-
-    print("Generating your public/private keypairs now . . .")
-    public, private = generate_keypair(p, q)
-
-    print("Your public key is ", public, " and your private key is ", private)
-    message = input("Enter a message to encrypt with your private key: ")
-    print("")
-
-    hashed = hashFunction(message)
-
-    print("Encrypting message with private key ", private, " . . .")
-    encrypted_msg = encrypt(private, hashed)
-    print("Your encrypted hashed message is: ")
-    print(''.join(map(lambda x: str(x), encrypted_msg)))
-    # print(encrypted_msg)
-
-    print("")
-    print("Decrypting message with public key ", public, " . . .")
-
-    decrypted_msg = decrypt(public, encrypted_msg)
-    print("Your decrypted message is:")
-    print(decrypted_msg)
-
-    print("")
-    print("Verification process . . .")
-    verify(decrypted_msg, message)
-
-
-main()
+if __name__ == "__main__":
+    rsa_sign()
